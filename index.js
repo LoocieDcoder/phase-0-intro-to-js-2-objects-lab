111b427eb00cfb735be3c8c22c19df21b11ddfe9
Lucy <quaysonlucyofficial@gmail.com>
2022-05-18 06:55:45 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const employee = {
-    name : "Lucy",
-    streetAddress : "11 Broadway"
-  };
-  
-  
+  name : "Lucy",
+  streetAddress : "Las Vegas 2"
+};
+
+
+
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObject = {...employee};
-    newObject[key]=value;
-    return newObject;
-  }
-  
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    const newObject = employee;
-    newObject[key]=value;
-    return newObject;
-  }
-  
-  function deleteFromEmployeeByKey(employee,key){
-    const newObject = {...employee};
-    delete newObject[key];
-    return newObject;
-  }
+  const newObject = {...employee};
+  newObject[key]=value;
+  return newObject;
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, "name", "Michael"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  const newObject = employee;
+  newObject[key]=value;
+  return newObject;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+  const newObject = {...employee};
+  delete newObject[key];
+  return newObject;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee,"name"));
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  const newObject = employee;
+  delete newObject[key];
+  return newObject;
+}
